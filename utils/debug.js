c8165d8a5d7caa4b49fd46a60d441ea644bcfadf
johnthomsonn <johnthomson16@hotmail.co.uk>
2020-05-13 21:53:12 +0100

    add yellow log

--- utils/debug.js
+++ utils/debug.js
@@ -2,3 +2,4 @@ const chalk = require('chalk')
 
 exports.error = (err) => console.log(chalk.red(err));
 exports.debug = (msg) => console.log(chalk.blue(msg));
+exports.yellow = (msg) => console.log(chalk.yellow(msg));
