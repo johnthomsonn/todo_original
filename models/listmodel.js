fe80e49b3d01bf9369c28f0c523ca9dc9c117193
johnthomsonn <johnthomson16@hotmail.co.uk>
2020-05-13 09:19:18 +0100

    list model

--- models/listmodel.js
+++ models/listmodel.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const {ObjectId} = mongoose.Schema;
+const {itemSchema} = require('./usermodel')
 
 const listSchema = new mongoose.Schema({
   name : {
-    trype :String,
+    type :String,
     trim: true,
     required : true
-  }
+  },
+  items : [itemSchema]
 })
+
+module.exports = mongoose.model("List", listSchema);
