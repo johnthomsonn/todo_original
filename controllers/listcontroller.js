04a6969ab7c1b3ddbf2d841963f673706a0eb8bd
johnthomsonn <johnthomson16@hotmail.co.uk>
2020-05-13 09:31:26 +0100

    start of get/post lists

--- controllers/listcontroller.js
+++ controllers/listcontroller.js
@@ -1,1 +1,9 @@
 const express = require('express');
+
+exports.getLists = (req,res) => {
+  res.send("getting all lists for user: " + req.user.username)
+}
+
+exports.createList = (req,res) => {
+  res.send("creating a list with name " + req.body.name + " for " + req.user.username)
+}
