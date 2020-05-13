32dc433483e6efb33981493b9395206794a92bbf
johnthomsonn <johnthomson16@hotmail.co.uk>
2020-05-13 08:52:50 +0100

    add item model

---
+++ models/itemmodel.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const itemSchema = new mongoose.Schema({
+  content :{
+    type:String,
+    required:true,
+    trim:true
+  },
+  completed : {
+    type : Boolean,
+    default : false
+  }
+})
+
+itemSchema.methods = {
+  setCompleted : function()
+  {
+    this.completed = true;
+  },
+  undoCompleted : function()
+  {
+    this.completed = false;
+  }
+}
+
+module.exports = mongoose.model('Item', itemSchema)
