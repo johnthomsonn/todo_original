ceb63c943af6329b3f27752bbbaca87d2c298196
johnthomsonn <johnthomson16@hotmail.co.uk>
2020-05-14 10:12:03 +0100

    somehow deleted models folder in previous commit. back now

---
+++ models/listmodel.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const {ObjectId} = mongoose.Schema;
+
+const listSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    trim: true,
+    required: true
+  },
+  items: [{type: ObjectId, ref: "Item"}]
+});
+
+module.exports = mongoose.model("List", listSchema);
