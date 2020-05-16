5108723515c7d48db7e6dcd2c00862e37ac771d8
johnthomsonn <johnthomson16@hotmail.co.uk>
2020-05-16 22:34:13 +0100

    altered order of components

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import {BrowserRouter} from 'react-router-dom'
 import MainRouter from "./components/core/MainRouter"
-import NavBar from './components/main//NavBar/NavBar'
 
 function App() {
   return (
     <div>
     <BrowserRouter>
-    <NavBar />
       <MainRouter />
     </BrowserRouter>
     </div>
