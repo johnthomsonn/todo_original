59c8a37f5f6eabf594357b52e4cf9dd941ffda30
johnthomsonn <johnthomson16@hotmail.co.uk>
2020-05-16 22:34:25 +0100

    start of hoem page

--- react-frontend/src/components/pages/Home.js
+++ react-frontend/src/components/pages/Home.js
@@ -1,12 +1,21 @@
-import React from 'react'
-
+import React from "react";
+import NavBar from "../main/NavBar/NavBar";
 const Home = () => {
-
   return (
-    <div>
-      Home page
-    </div>
+    <>
+      <NavBar />
+
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-12 todo-create">Create a new todo item</div>
+        </div>
+
+        <div className="row todo-items">
+          <div className="col-md-3">some items</div>
+        </div>
+      </div>
+    </>
   );
-}
+};
 
 export default Home;
