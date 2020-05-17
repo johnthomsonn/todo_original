594696235266cd1c4ac4991dacbf27cae8f68ac0
johnthomsonn <johnthomson16@hotmail.co.uk>
2020-05-17 13:04:41 +0100

    add todo design

--- react-frontend/src/components/pages/Home.js
+++ react-frontend/src/components/pages/Home.js
@@ -1,14 +1,25 @@
 import React from "react";
 import NavBar from "../main/NavBar/NavBar";
+import HomeCss from './Home.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPencilAlt as pencil } from '@fortawesome/free-solid-svg-icons'
 const Home = () => {
   return (
     <>
       <NavBar />
 
       <div className="container-fluid">
-        <div className="row">
-          <div className="col-12 todo-create">Create a new todo item</div>
+        <div className="row justify-content-center">
+          <div className="col-3 todo-create mt-5">
+            <form className="todo-text flex-inline">
+              <input type="text" placeholder="write something" />
+              <button type="button" style={{backgroundColor: "#eddf91",  height: "130%"}}  class="btn btn-primary bmd-btn-fab todo-create-pencil-btn">
+        <i class="material-icons" ><FontAwesomeIcon icon={pencil} color="black" className="fa-pencil"/></i>
+      </button>
+            </form>
+          </div>
         </div>
+
 
         <div className="row todo-items">
           <div className="col-md-3">some items</div>
