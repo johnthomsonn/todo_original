6c12fdeca901ee8e6a855256bc65b923f30f8448
johnthomsonn <johnthomson16@hotmail.co.uk>
2020-05-17 13:13:01 +0100

    start of sign up page

--- react-frontend/src/components/pages/Signup.js
+++ react-frontend/src/components/pages/Signup.js
@@ -1,11 +1,17 @@
 import React from 'react'
+import NavBar from '../main/NavBar/NavBar'
+import SignupCss from './Signup.css'
 
 const Signup = () =>{
 
   return (
-    <div>
-      Sign up page
-    </div>
+    <>
+    <NavBar />
+      <div className="container sign-up-container">
+        <h1 className="header">Sign Up</h1>
+
+      </div>
+    </>
   );
 }
 
