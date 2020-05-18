38e0ce5a2461c8020e68b69e2d20fff24783f64a
johnthomsonn <johnthomson16@hotmail.co.uk>
2020-05-18 11:02:16 +0100

    check if logged in

--- react-frontend/src/auth/Auth.js
+++ react-frontend/src/auth/Auth.js
@@ -2,4 +2,16 @@
 
 export const cleanInput = input => !/[[\]<>^!@"()#}/{+\s]/.test(input)
 
-  export const validateEmail = email => /.+@.+\..+/.test(email)
+export const validateEmail = email => /.+@.+\..+/.test(email)
+
+export const isLoggedIn = () => {
+  fetch("http://localhost:5000/auth/islogged")
+  .then(res => res.json())
+  .then(data => {
+    if(data.logged)
+      return true;
+      else
+      return false;
+  })
+  .catch(err => false)
+}
