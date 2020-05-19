0582dc4db9d59f12c8bf8858fddaa130965ded68
johnthomsonn <johnthomson16@hotmail.co.uk>
2020-05-19 09:57:42 +0100

    basic sign in page template

--- react-frontend/src/components/core/MainRouter.js
+++ react-frontend/src/components/core/MainRouter.js
@@ -2,6 +2,7 @@ import React from 'react'
 import {Route, Switch} from 'react-router-dom'
 import Home from '../pages/Home'
 import Signup from '../pages/Signup'
+import Signin from '../pages/Signin'
 import Error404 from '../pages/404'
 
 const MainRouter = () => {
@@ -10,6 +11,7 @@ const MainRouter = () => {
       <Switch>
         <Route path="/" exact  component={Home} />
         <Route path="/signup" exact component={Signup} />
+        <Route path="/signin" exact component={Signin} />
         <Route path="/" component={Error404} />
       </Switch>
     </div>
---
+++ react-frontend/src/components/pages/Signin.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import NavBar from '../main/NavBar/NavBar'
+const Signin = () => {
+
+
+  return (
+    <>
+      <NavBar />
+    </>
+  );
+}
+
+export default Signin;
