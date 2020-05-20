186fb12466dfc01704c9bb8a0cf3430313b12e17
johnthomsonn <johnthomson16@hotmail.co.uk>
2020-05-20 14:31:59 +0100

    update home page style

--- react-frontend/src/components/pages/Home.js
+++ react-frontend/src/components/pages/Home.js
@@ -2,16 +2,13 @@ import React, {useEffect} from "react";
 import NavBar from "../main/NavBar/NavBar";
 import './Home.css'
 import CreateTodo from '../main/CreateTodo'
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faCheck as tick} from "@fortawesome/free-solid-svg-icons";
 
 const Home = (props) => {
 
-useEffect( () => {
-  checkCookie();
-}, [])
-
-
-const checkCookie = () => {
-    const authtoken = document.cookie
+const clickedTodo = event => {
+  event.target.classList.toggle("checked")
 }
 
 if(props.match.params.error != null)
@@ -23,16 +20,27 @@ if(props.match.params.error != null)
       <NavBar history={props.history} />
 
       <div className="container-fluid">
-        <div className="row justify-content-center">
-          <CreateTodo />
-        </div>
+
+      <section id="header">
+        <p className="header-title">ToDo Lists</p>
+
+        <p className="header-sub-title">Organising your day <FontAwesomeIcon icon={tick} className="tick" /></p>
+
+      </section>
+<hr />
+      <section id="main" className="">
+        <p className="about-text">
+          <ul>
+          <li onClick={clickedTodo}>Create your own private account</li>
+          <li onClick={clickedTodo}>Create as many todo lists as you need</li>
+          <li onClick={clickedTodo}>Create specific todo items for each list</li>
+          <li onClick={clickedTodo}>Check or uncheck as and when you need</li>
+          <li onClick={clickedTodo}>Remove checked items when you are finished</li>
+          </ul>
+        </p>
+      </section>
 
 
-
-
-        <div className="row todo-items">
-          <div className="col-md-3">some items</div>
-        </div>
       </div>
     </>
   );
