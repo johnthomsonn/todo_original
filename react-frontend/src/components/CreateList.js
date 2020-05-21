e481f2a470ab44c6fe5ce1e036b56615ed428faf
johnthomsonn <johnthomson16@hotmail.co.uk>
2020-05-21 20:07:34 +0100

    minor style tweaks

--- react-frontend/src/components/CreateList.js
+++ react-frontend/src/components/CreateList.js
@@ -1,42 +1,64 @@
-import React, {useState} from 'react'
-import './CreateList.css'
+import React, {useState} from "react";
+import "./CreateList.css";
 
-const CreateList = (props) => {
+const CreateList = props => {
+  const [listName, setListName] = useState("");
+  const [invalid, setInvalid] = useState([]);
 
- const [listName, setListName] = useState("")
+  const onFormSubmit = evt => {
+    evt.preventDefault();
+    alert("clicked")
+  };
 
-const onFormSubmit =(evt) => {
-  evt.preventDefault();
+  const handleChange = event => {
+    const name = event.target.value;
+    const invalidChars = name.match(/[*^<>'"]/gi);
+    if (invalidChars) {
+      setInvalid(invalidChars);
+    } else {
+      setInvalid([]);
+    }
+    setListName(event.target.value);
+  };
 
+  const showCreateList = () => {
+    return (
+      <>
+        <h4>Create a new list</h4>
 
-}
-
-const handleChange = event => {
-  setListName(event.target.value)
-}
-
-const showCreateList = () => {
-  return (<>
-    <h4>Create a new list</h4>
-
-    <form className="create-form" onSubmit={onFormSubmit}>
-      <input type="text" name="name" value={listName} onChange={handleChange} placeholder="Unique list name" />
-      <button type="submit" className="btn  btn-raised  btn-outline" disabled={(!listName.length > 0 ? false : true)}>
-      Create List
-      </button>
-    </form>
-
-
-    </>);
-}
+        <form className="create-form" onSubmit={onFormSubmit}>
+          <input
+            type="text"
+            name="name"
+            value={listName}
+            onChange={handleChange}
+            placeholder="Unique list name"
+          />
+          <button
+            type="submit"
+            className="btn  btn-raised  btn-outline"
+            disabled={listName.length > 0 && invalid.length == 0 ? false : true}
+          >
+            Create List
+          </button>
+        </form>
+      </>
+    );
+  };
 
   return (
-      <div className="create-list">
-        {props.user.username != "" && showCreateList()}
+    <div className="create-list">
+      {props.user.username != "" && showCreateList()}
+
+      <div
+        className="alert alert-danger"
+        style={{display: invalid.length > 0 ? "" : "none", fontSize : "0.8rem"}}
+      >
+      List name cannot contain {invalid}
+      </div>
+
     </div>
-  )
-}
-
-
+  );
+};
 
 export default CreateList;
