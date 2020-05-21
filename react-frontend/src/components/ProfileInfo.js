855be214e288b22228b5687f22a661846aa6bea4
johnthomsonn <johnthomson16@hotmail.co.uk>
2020-05-21 09:58:58 +0100

    profile info componenet

---
+++ react-frontend/src/components/ProfileInfo.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import './ProfileInfo.css'
+
+const ProfileInfo = props => {
+
+return (<>
+
+ <div className="profile-info-box">
+
+ {props.test}
+    <p>
+    {props.username}
+    </p>
+
+    <p>
+    </p>
+
+    <p>
+    </p>
+ </div>
+
+</>);
+}
+
+
+export default ProfileInfo;
