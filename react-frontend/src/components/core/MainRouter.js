933dfb5c3f52edbdca9820a9c92ea23e9a57619a
johnthomsonn <johnthomson16@hotmail.co.uk>
2020-05-22 20:25:58 +0100

    add in List view page

--- react-frontend/src/components/core/MainRouter.js
+++ react-frontend/src/components/core/MainRouter.js
@@ -4,8 +4,10 @@ import Home from '../pages/Home'
 import Signup from '../pages/Signup'
 import Signin from '../pages/Signin'
 import Profile from '../pages/Profile'
+import ListPage from '../pages/ListPage'
 import Error404 from '../pages/404'
 import PrivateRoute from "./PrivateRoute"
+
 
 const MainRouter = () => {
   return (
@@ -15,7 +17,7 @@ const MainRouter = () => {
         <Route path="/signup" exact component={Signup} />
         <Route path="/signin" exact component={Signin} />
         <PrivateRoute path="/:username" exact component={Profile} />
-        <PrivateRoute path="/:username/:list" exact component={Profile} />
+        <PrivateRoute path="/:username/:list" exact component={ListPage} />
         <Route path="/" component={Error404} />
       </Switch>
     </div>
