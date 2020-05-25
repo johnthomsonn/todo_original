435f92b13750ed3ae5ed8e8883cf8ed92fc52293
johnthomsonn <johnthomson16@hotmail.co.uk>
2020-05-25 18:58:31 +0100

    list name cannot contain numbers

--- react-frontend/src/components/CreateList.js
+++ react-frontend/src/components/CreateList.js
@@ -49,7 +49,7 @@ try {
 
   const handleChange = event => {
     const name = event.target.value;
-    const invalidChars = name.match(/[*^<>@'/\\"]/gi);
+    const invalidChars = name.match(/[*^<>@'/\\"\d]/gi);
     if (invalidChars) {
       setInvalid(invalidChars);
     } else {
