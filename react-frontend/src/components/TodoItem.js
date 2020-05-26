5efbf3dbc74673f9961fcd596bd3a4409d6c17ba
johnthomsonn <johnthomson16@hotmail.co.uk>
2020-05-26 10:52:41 +0100

    can now check/uncheck items

--- react-frontend/src/components/TodoItem.js
+++ react-frontend/src/components/TodoItem.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import './TodoItem.css'
 
 const TodoItem = props => {
 
+const [id,setID] = useState("")
+  useEffect(() => setID(props.id), [])
+  let styles = 'item-container'
+
+  const checkTodo = (evt) => {
+    // evt.target.classList.toggle("checked")
+    document.getElementById(`${id}`).classList.toggle("checked")
+  }
 
   return (<>
-    <div className="item-container">
+    <div className={styles} onClick={checkTodo} id={id}>
       <p className="todo-content">
         {props.content}
       </p>
