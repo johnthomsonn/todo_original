36f4a9a6c0de71701e6ee7c9df220b2afb25314e
johnthomsonn <johnthomson16@hotmail.co.uk>
2020-05-28 09:17:44 +0100

    start of remove completed items

---
+++ react-frontend/src/components/RemoveCompleted.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './RemoveCompleted.css'
+const RemoveCompleted = props => {
+
+  return (<>
+    <div className="remove-completed-div">
+
+
+    Remove completed
+
+    </div>
+  </>)
+}
+
+export default RemoveCompleted
