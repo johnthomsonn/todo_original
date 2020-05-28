925f2b25cfbdcdbefe4a1ad2aadd3018257dfe1f
johnthomsonn <johnthomson16@hotmail.co.uk>
2020-05-28 09:34:30 +0100

    style

--- react-frontend/src/components/RemoveCompleted.js
+++ react-frontend/src/components/RemoveCompleted.js
@@ -1,13 +1,26 @@
 import React from 'react'
 import './RemoveCompleted.css'
+
+
 const RemoveCompleted = props => {
+
+const btnStyle = {
+  border: "2px solid black",
+  backgroundColor : "#eddf91",
+  boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)",
+  fontFamily: "Balsamiq Sans, cursive"
+}
+
+  const remove = () =>{
+    let itemsToRemove = props.items.filter(item => item.completed === true)
+  }
 
   return (<>
     <div className="remove-completed-div">
 
-
-    Remove completed
-
+    <button onClick={remove} className="remove-button btn btn-outline btn-raised" style={btnStyle}>
+      Remove completed items
+    </button>
     </div>
   </>)
 }
